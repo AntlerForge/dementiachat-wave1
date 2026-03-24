d4712d3011d30b3151013ad74cf5f7afd3a5dc4a
Antony Barfoot <tonybarfoot@mac.com>
2026-03-24 18:48:13 +0000

    Add deploy config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://fgxyujxaolzcxuufhlmz.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ZwdgKEn1PYWQUTqE7DWU4A_FyVBbv6s",
+  SHARED_CONVERSATION_ID: "17f4524c-0e78-4798-b10c-ab8e7b9f0b7f",
+  OUTBOX_SYNC_MS: 5000,
+  DEFAULT_DELAY_SECONDS: 180,
+  MAX_DELAY_SECONDS: 900,
+};
